7cdcdb6f705c1a000701b4a781ed2df8a1ec5a8f
ChandulaSenevirathna <97549694+ChandulaSenevirathna@users.noreply.github.com>
2023-02-11 14:27:56 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -209,13 +209,12 @@ $deskripsi = implode('<br>',$deskripsi_explode);
             
             
 
-            <input type="hidden" name="harga_barang" value="<?php echo $harga_barang; ?>" >
+<!-- <input type="hidden" name="harga_barang" value="<?php //echo $harga_barang; ?>" > -->
+            
+            <input type="hidden" name="harga_barang" value="<?php echo htmlspecialchars($harga_barang, ENT_QUOTES, 'UTF-8'); ?>" >
+
+
    <input type="hidden" name="nama_barang" value="<?php echo $nama_barang; ?>" >
-            
-            
-            
-        
-     <input type="hidden" name="md5item" value="<?php echo htmlspecialchars($md5item, ENT_QUOTES, 'UTF-8');?>">
           
             <p>Jumlah Barang :</p>
             <input class="inputkuwkwk" type="number" name="jumlah_pesanan" placeholder="contoh: 1" required>
